a6aaa53e22ce07775197682d68f60a28df8041db
MilenkoOlena <consuertelm@gmail.com>
2026-05-17 17:56:29 +0300

    MNFCT-2 added detenv library for configurations

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,8 +1,16 @@
+import os
+from dotenv import load_dotenv
+
 from playwright.sync_api import sync_playwright, Page
 from playwright.sync_api import Page, expect
 
+load_dotenv()
+
+LOGIN_URL=f"{os.getenv('BASE_APP_URL')}/users/sign_in"
+EMAIL=os.getenv("EMAIL")
+PASSWORD=os.getenv("PASSWORD")
 def test_login_with_invalid_creds(page: Page):
-    login_user(page=page, mail="consuertelm@gmail.com", password="Reyna2017milenko")
+    login_user(page=page, mail=EMAIL, password=PASSWORD)
 
     page.goto("https://testomat.io")
     expect(page.locator("[href*='sign_in'].login-item")).to_be_visible()
@@ -18,67 +26,73 @@ def test_login_with_invalid_creds(page: Page):
     expect(page.locator("#content-desktop").get_by_text("Invalid Email or password.")).to_be_visible()
     expect(page.locator("#content-desktop").get_by_text("Invalid Email or password.")).to_be_visible()
 
+
 def test_search_project_in_company(page: Page):
-    page.goto("https://app.testomat.io/users/sign_in")
+    page.goto(LOGIN_URL, timeout=60000)
 
+    # page.get_by_text("Log in", exact=True).click()
 
-   # page.get_by_text("Log in", exact=True).click()
+    # page.locator("#content-desktop #user_email").fill("consuertelm@gmail.com")
+    # page.locator("#content-desktop #user_password").fill("ktkckyl")
+    # page.get_by_role(role="button", name="Sign in").click()
 
-   # page.locator("#content-desktop #user_email").fill("consuertelm@gmail.com")
-   # page.locator("#content-desktop #user_password").fill("ktkckyl")
-   # page.get_by_role(role="button", name="Sign in").click()
-
-    login_user(page=page, mail="consuertelm@gmail.com", password="Reyna2017milenko")
+    login_user(page=page, mail=EMAIL, password=PASSWORD)
     print(page.url)
-    target_project="MyProjectTaras"
+    target_project = "MyProjectTaras"
     search_for_project(page=page, target_project=target_project)
-   # expect(page.get_by_role("searchbox", name="Search")).to_be_visible()
+    # expect(page.get_by_role("searchbox", name="Search")).to_be_visible()
     # page.locator("#content-desktop #search").fill(target_project)
 
     expect(page.get_by_role("heading", name=target_project)).to_be_visible()
-    #expect(page.locator("ul li h3").filter(has_text=target_project)).to_have_text(target_project)
+    # expect(page.locator("ul li h3").filter(has_text=target_project)).to_have_text(target_project)
 
-def search_for_project(page: Page, target_project:str):
+
+def search_for_project(page: Page, target_project: str):
     expect(page.get_by_role("searchbox", name="Search")).to_be_visible()
     page.locator("#content-desktop #search").fill(target_project)
 
+
 def test_should_be_possible_to_open_free_project(page: Page):
-    #arrenge
-    page.goto("https://app.testomat.io/users/sign_in")
-    login_user(page=page, mail="consuertelm@gmail.com", password="Reyna2017milenko")
-    #act
+    # arrenge
+    page.goto(LOGIN_URL,timeout=6000)
+    login_user(page=page, mail=EMAIL, password=PASSWORD)
+    # act
     page.locator("#company_id").click()
     page.locator("#company_id").select_option("Free Projects")
 
-    #assert
+    # assert
     target_project = "MyProjectTaras"
     search_for_project(page=page, target_project=target_project)
     expect(page.get_by_role("heading", name=target_project)).to_be_hidden()
 
     expect(page.get_by_text("You have not created any projects yet")).to_be_visible(timeout=10000)
 
+
 def open_home_page(page: Page):
-    page.goto("https://testomat.io")
+    page.goto(os.getenv("BASE_URL"))
 
 
 
 def login_user(page, mail, password):
-    page.goto("https://app.testomat.io/users/sign_in")
 
-    page.set_viewport_size({"width": 1280, "height": 800})
+    email_input = page.locator("input[name='user[email]']:visible")
+    password_input = page.locator("input[name='user[password]']:visible")
+    sign_in_button = page.locator("input[value='Sign In']:visible")
 
-    container = page.locator("#content-desktop:visible")
-
-    email = container.locator("#user_email")
-    password_input = container.locator("#user_password")
-    button = container.locator("input[name='commit']")
-
-    email.fill(mail)
+    email_input.fill(mail)
     password_input.fill(password)
 
-    password_input.press("Tab")
+    expect(sign_in_button).to_be_enabled()
 
-    expect(button).to_be_visible()
-    expect(button).to_be_enabled()
+    sign_in_button.click()
 
-    button.click()
+
+    #email.fill(mail)
+    #password_input.fill(password)
+
+    #password_input.press("Tab")
+
+    #expect(button).to_be_visible()
+    #expect(button).to_be_enabled()
+
+    #button.click()
